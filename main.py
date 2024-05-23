be8b4ebaa1b9de79c3452b21f1abd22c2986acad
Feng, Xixuan (Aaron) <aaron@megagon.ai>
2024-05-23 17:49:35 +0000

    minor

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     conn_str = f'postgresql+psycopg://{user}@{host}/{db}'
     engine = create_engine(conn_str)
     with engine.connect() as connection:
-        sql = "SELECT player FROM wikisql.t_1_10015132_1 LIMIT 3"
+        sql = "SELECT player FROM nba_wikisql.t_1_10015132_1 LIMIT 3"
         result = connection.execute(text(sql))
         for row in result:
             print("Player:", row.player)
