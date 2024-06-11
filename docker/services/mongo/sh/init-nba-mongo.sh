0be955880e9bf8951b3c40b1376a1befc95f6da2
Yanlin <yanlinf23@gmail.com>
2024-06-11 13:53:00 +0800

    Load tables into MongoDB

--- docker/services/mongo/sh/init-nba-mongo.sh
+++ docker/services/mongo/sh/init-nba-mongo.sh
@@ -1,1 +1,2 @@
 mongoimport --host localhost --port 27017 --db nba-datalake --collection wiki-documents --type json --file /init/nba-datalake.wiki-documents.json --jsonArray
+mongoimport --host localhost --port 27017 --db nba-datalake --collection wiki-tables --type json --file /init/nba-datalake.wiki-tables.json --jsonArray
